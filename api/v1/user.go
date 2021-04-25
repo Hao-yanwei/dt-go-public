5e003affbf2e849b16a94db213e36b421dc089e3
963936487@qq.com <hao963936487>
2021-04-25 08:39:43 +0800

    完成博客分类， 文章增删改查接口

--- api/v1/user.go
+++ api/v1/user.go
@@ -6,6 +6,8 @@ import (
 	"dt-go-public/model"
 	"dt-go-public/utils/errmsg"
 	"github.com/gin-gonic/gin"
+	"strconv"
+
 	//"github.com/go-playground/validator/v10"
 	"net/http"
 	//"github.com/go-playground/validator/v10"
@@ -50,24 +52,102 @@ func AddUser(c *gin.Context) {
 	)
 }
 
-// 查询用户是否存在
-func UserExist(c *gin.Context) {
-
+// GetUserInfo 查询单个用户
+func GetUserInfo(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	// 构建一个map
+	var maps = make(map[string]interface{})
+	data, code := model.GetUser(id)
+	maps["username"] = data.Username
+	maps["role"] = data.Role
+	c.JSON(
+		http.StatusOK, gin.H{
+			"status":  code,
+			"data":    maps,
+			"total":   1,
+			"message": errmsg.GetErrMsg(code),
+		},
+	)
 }
 
-// 查询用户列表
+// GetUsers 查询用户列表
 func GetUsers(c *gin.Context) {
+	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
+	pageNum, _ := strconv.Atoi(c.Query("pagenum"))
+	username := c.Query("username")
 
+	switch {
+	case pageSize >= 100:
+		pageSize = 100
+	case pageSize <= 0:
+		pageSize = 10
+	}
+
+	if pageNum == 0 {
+		pageNum = 1
+	}
+
+	data, total := model.GetUsers(username, pageSize, pageNum)
+
+	code = errmsg.SUCCSE
+	c.JSON(
+		http.StatusOK, gin.H{
+			"status":  code,
+			"data":    data,
+			"total":   total,
+			"message": errmsg.GetErrMsg(code),
+		},
+	)
 }
-
-// 查询单个用户
 
 // 编辑用户
 func EditUser(c *gin.Context) {
+	var data model.User
+	id, _ := strconv.Atoi(c.Param("id"))
+	_ = c.ShouldBindJSON(&data)
+	code := model.CheckUpUser(id, data.Username)
+	if code == errmsg.SUCCSE {
+		model.EditUser(id, &data)
+	}
+	if code == errmsg.ERROR_USERNAME_USED {
+		c.Abort()
+	}
 
+	c.JSON(
+		http.StatusOK, gin.H{
+			"status":  code,
+			"message": errmsg.GetErrMsg(code),
+		},
+	)
+
+}
+
+// 修改密码
+func ChangeUserPassword(c *gin.Context) {
+	var data model.User
+	id, _ := strconv.Atoi(c.Param("id"))
+	_ = c.ShouldBindJSON(&data)
+
+	code = model.ChangePassword(id, &data)
+
+	c.JSON(
+		http.StatusOK, gin.H{
+			"status":  code,
+			"message": errmsg.GetErrMsg(code),
+		},
+	)
 }
 
 // 删除用户
 func DeleteUser(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
 
+	code = model.DeleteUser(id)
+
+	c.JSON(
+		http.StatusOK, gin.H{
+			"status":  code,
+			"message": errmsg.GetErrMsg(code),
+		},
+	)
 }
--- routes/router.go
+++ routes/router.go
@@ -12,10 +12,21 @@ func InitRouter() {
 
 	router := r.Group("api/v1")
 	{
+		// 用户模块路由接口
 		router.POST("user/add", v1.AddUser)
-		router.GET("user/get", v1.GetUsers)
-		router.PUT("user/edit", v1.EditUser)
-		router.DELETE("user/del", v1.DeleteUser)
+		router.GET("user", v1.GetUsers)
+		router.PUT("user/:id", v1.EditUser)
+		router.DELETE("user/:id", v1.DeleteUser)
+		// 分类模块的路由接口
+		router.POST("category/add", v1.AddCategory)
+		router.GET("category", v1.GetCate)
+		router.PUT("category/:id", v1.EditCate)
+		router.DELETE("category/:id", v1.DeleteCate)
+		// 文章模块路由接口
+		router.POST("art/add", v1.AddArticle)
+		router.GET("art", v1.GetCateArt)
+		router.PUT("art/:id", v1.EditArt)
+		router.DELETE("art/:id", v1.DeleteArt)
 	}
 	r.Run(utils.HttpPort)
 }
