76a64af1292da8f2531ef76cb55e29b5e095a237
963936487@qq.com <hao963936487>
2021-04-27 23:26:51 +0800

    增加登录及对应增删改查接口权限认证

--- model/User.go
+++ model/User.go
@@ -140,3 +140,41 @@ func ScryptPw(password string) string {
 
 	return string(HashPw)
 }
+
+// CheckLogin 后台登录验证
+func CheckLogin(username string, password string) (User, int) {
+	var user User
+	var PasswordErr error
+
+	db.Where("username = ?", username).First(&user)
+
+	PasswordErr = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+
+	if user.ID == 0 {
+		return user, errmsg.ERROR_USER_NOT_EXIST
+	}
+	if PasswordErr != nil {
+		return user, errmsg.ERROR_PASSWORD_WRONG
+	}
+	if user.Role != 1 {
+		return user, errmsg.ERROR_USER_NO_RIGHT
+	}
+	return user, errmsg.SUCCSE
+}
+
+// CheckLoginFront 前台登录
+func CheckLoginFront(username string, password string) (User, int) {
+	var user User
+	var PasswordErr error
+
+	db.Where("username = ?", username).First(&user)
+
+	PasswordErr = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if user.ID == 0 {
+		return user, errmsg.ERROR_USER_NOT_EXIST
+	}
+	if PasswordErr != nil {
+		return user, errmsg.ERROR_PASSWORD_WRONG
+	}
+	return user, errmsg.SUCCSE
+}
--- routes/router.go
+++ routes/router.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	v1 "dt-go-public/api/v1"
+	"dt-go-public/middleware"
 	"dt-go-public/utils"
 	"github.com/gin-gonic/gin"
 )
@@ -10,23 +11,58 @@ func InitRouter() {
 	gin.SetMode(utils.AppMode)
 	r := gin.Default()
 
-	router := r.Group("api/v1")
+	/*
+		后台管理路由接口
+	*/
+	auth := r.Group("api/v1")
+	auth.Use(middleware.JwtToken())
 	{
 		// 用户模块路由接口
-		router.POST("user/add", v1.AddUser)
-		router.GET("user", v1.GetUsers)
-		router.PUT("user/:id", v1.EditUser)
-		router.DELETE("user/:id", v1.DeleteUser)
+
+		auth.PUT("user/:id", v1.EditUser)
+		auth.DELETE("user/:id", v1.DeleteUser)
 		// 分类模块的路由接口
-		router.POST("category/add", v1.AddCategory)
-		router.GET("category", v1.GetCate)
-		router.PUT("category/:id", v1.EditCate)
-		router.DELETE("category/:id", v1.DeleteCate)
+		auth.POST("category/add", v1.AddCategory)
+		auth.PUT("category/:id", v1.EditCate)
+		auth.DELETE("category/:id", v1.DeleteCate)
 		// 文章模块路由接口
-		router.POST("art/add", v1.AddArticle)
-		router.GET("art", v1.GetCateArt)
-		router.PUT("art/:id", v1.EditArt)
-		router.DELETE("art/:id", v1.DeleteArt)
+		auth.POST("art/add", v1.AddArticle)
+		auth.PUT("art/:id", v1.EditArt)
+		auth.DELETE("art/:id", v1.DeleteArt)
 	}
+
+	/*
+		前端展示页面接口
+	*/
+	router := r.Group("api/v1")
+	{
+		// 用户信息模块
+		router.POST("user/add", v1.AddUser)
+		router.GET("user/:id", v1.GetUserInfo)
+		router.GET("users", v1.GetUsers)
+
+		// 文章分类信息模块
+		router.GET("category", v1.GetCate)
+		router.GET("category/:id", v1.GetCateInfo)
+
+		// 文章模块
+		router.GET("art", v1.GetArt)
+		router.GET("art/list/:id", v1.GetCateArt)
+		router.GET("art/info/:id", v1.GetArtInfo)
+
+		// 登录控制模块
+		router.POST("login", v1.Login)
+		router.POST("loginfront", v1.LoginFront)
+
+		//// 获取个人设置信息
+		//router.GET("profile/:id", v1.GetProfile)
+		//
+		//// 评论模块
+		//router.POST("addcomment", v1.AddComment)
+		//router.GET("comment/info/:id", v1.GetComment)
+		//router.GET("commentfront/:id", v1.GetCommentListFront)
+		//router.GET("commentcount/:id", v1.GetCommentCount)
+	}
+
 	r.Run(utils.HttpPort)
 }
